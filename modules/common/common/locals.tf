301fd008b89a4c9d6402ae29720f61b42c7dc062
Martin Koldovsky <mkoldov@gmail.com>
2026-01-19 13:56:29 +0100

    formatting

--- modules/common/common/locals.tf
+++ modules/common/common/locals.tf
@@ -1,5 +1,3 @@
 locals {
-  resource_group = var.resource_group_create
-    ? azurerm_resource_group.this[0]
-    : data.azurerm_resource_group.this[0]
+  resource_group = var.resource_group_create ? azurerm_resource_group.this[0] : data.azurerm_resource_group.this[0]
 }
